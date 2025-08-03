e3a361248b44aa20d8e11e4f571977327e628146
sandeeffendi <sande.effendi@gmail.com>
2025-08-03 22:51:49 +0700

    Fix: Keyboard Overflow

--- lib/screens/mobile/login_page.dart
+++ lib/screens/mobile/login_page.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:my_submission_app/components/my_text_form_field.dart';
@@ -92,228 +90,157 @@ class _MyLoginPageState extends State<MyLoginPage> {
     super.dispose();
   }
 
-  Widget MyBuild(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(backgroundColor: Colors.transparent),
-      extendBodyBehindAppBar: true,
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              children: [
-                Form(
-                  key: _keyForm,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      // Logo
-                      Center(child: Icon(Icons.chat_rounded, size: 100)),
-
-                      SizedBox(height: 8),
-
-                      // Welcome Back
-                      Text(
-                        'Welcome back. you\'ve been miseed.',
-                        style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                          color: Theme.of(context).colorScheme.secondary,
-                        ),
-                      ),
-
-                      SizedBox(height: 8),
-
-                      // Email Input
-                      MyTextFormField(
-                        validator: _validateEmail,
-                        hintText: "Email",
-                        controller: _emailController,
-                      ),
-
-                      SizedBox(height: 8),
-
-                      // Password Input
-                      MyTextFormField(
-                        validator: _validatePassword,
-                        hintText: "Password",
-                        obscure: true,
-                        controller: _passwordController,
-                      ),
-
-                      SizedBox(height: 15),
-
-                      // Login Button
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 20),
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: _submitLoginForm,
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Theme.of(
-                              context,
-                            ).colorScheme.primary,
-                            foregroundColor: Theme.of(
-                              context,
-                            ).colorScheme.secondary,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-
-                          child: Text(
-                            'Login',
-                            style: Theme.of(context).textTheme.titleLarge,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                SizedBox(height: 15),
-
-                // Dont have an Account? Create An Account.
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Don\'t have an account?',
-                      style: GoogleFonts.montserrat(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w500,
-                        color: Theme.of(context).colorScheme.secondary,
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Create an account.',
-                        style: GoogleFonts.montserrat(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w600,
-                          color: Theme.of(context).colorScheme.secondary,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // Image Display Container
-          Expanded(
-            flex: 4,
-            child: Container(
-              padding: EdgeInsets.only(left: 37, right: 37, top: 155),
-              color: Theme.of(context).colorScheme.primary,
-              width: double.infinity,
-              child: Column(
-                children: [
-                  Image.asset('assets/images/welcome_screen_basket.png'),
-                  SizedBox(height: 8),
-                  Image.asset('assets/images/welcome_screen_basket_shadow.png'),
-                ],
-              ),
-            ),
-          ),
+      resizeToAvoidBottomInset: true,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          final isKeyboardView = MediaQuery.of(context).viewInsets.bottom > 0;
 
-          // Form Display Container
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: EdgeInsets.only(top: 8, bottom: 8, left: 24, right: 24),
-              child: Column(
-                children: [
-                  Text(
-                    'Login To Explore More',
-                    style: Theme.of(context).textTheme.bodyLarge,
-                  ),
+          final screenHeight = MediaQuery.of(context).size.height;
+          final keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
+          final availableHeight = screenHeight - keyboardHeight;
 
-                  // Login Form
-                  SizedBox(height: 8),
-                  Form(
+          final topHeight = availableHeight * 4 / 7;
+          final bottomHeight = availableHeight * 3 / 7;
+
+          return SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Theme.of(context).colorScheme.primary,
+                  width: double.infinity,
+                  height: topHeight,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 37, right: 37, top: 155),
                     child: Column(
                       children: [
-                        //Email Input
-                        MyTextFormField(
-                          hintText: 'Email',
-                          controller: _emailController,
-                        ),
-
-                        SizedBox(height: 8),
-
-                        // Password Input
-                        MyTextFormField(
-                          hintText: 'Password',
-                          controller: _emailController,
-                        ),
-
-                        SizedBox(height: 8),
-
-                        // Login Button
                         SizedBox(
-                          width: double.infinity,
-                          height: 56,
-                          child: RawMaterialButton(
-                            onPressed: () {},
-                            fillColor: Theme.of(context).colorScheme.primary,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                            child: Text(
-                              'Login',
-                              style: Theme.of(context).textTheme.labelLarge
-                                  ?.copyWith(color: Colors.white),
-                            ),
+                          height: isKeyboardView ? 50 : 260,
+                          child: Image.asset(
+                            'assets/images/welcome_screen_basket.png',
                           ),
                         ),
-
-                        // Dont have an account? Create an Account
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(
-                              'Don\'t have an account?',
-                              style: GoogleFonts.montserrat(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w400,
-                                color: Theme.of(context).colorScheme.secondary,
-                              ),
-                            ),
-                            TextButton(
-                              onPressed: () {},
-                              child: Text(
-                                'Create an account.',
-                                style: GoogleFonts.montserrat(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w600,
-                                  color: const Color.fromARGB(255, 73, 32, 255),
-                                ),
-                              ),
-                            ),
-                          ],
+                        SizedBox(height: 8),
+                        Image.asset(
+                          'assets/images/welcome_screen_basket_shadow.png',
                         ),
                       ],
                     ),
                   ),
-                ],
-              ),
+                ),
+
+                // Form Display Container
+                SizedBox(
+                  height: bottomHeight,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      top: 8,
+                      bottom: 8,
+                      left: 24,
+                      right: 24,
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Login To Explore More',
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        ),
+
+                        // Login Form
+                        SizedBox(height: 8),
+                        Form(
+                          key: _keyForm,
+                          child: Column(
+                            children: [
+                              //Email Input
+                              MyTextFormField(
+                                hintText: 'Email',
+                                validator: _validateEmail,
+                                controller: _emailController,
+                              ),
+
+                              SizedBox(height: 8),
+
+                              // Password Input
+                              MyTextFormField(
+                                hintText: 'Password',
+                                validator: _validatePassword,
+                                controller: _passwordController,
+                                obscure: true,
+                              ),
+
+                              SizedBox(height: 8),
+
+                              // Login Button
+                              SizedBox(
+                                width: double.infinity,
+                                height: 56,
+                                child: RawMaterialButton(
+                                  onPressed: _submitLoginForm,
+                                  fillColor: Theme.of(
+                                    context,
+                                  ).colorScheme.primary,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                  child: Text(
+                                    'Login',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .labelLarge
+                                        ?.copyWith(color: Colors.white),
+                                  ),
+                                ),
+                              ),
+
+                              // Dont have an account? Create an Account
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    'Don\'t have an account?',
+                                    style: GoogleFonts.montserrat(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400,
+                                      color: Theme.of(
+                                        context,
+                                      ).colorScheme.secondary,
+                                    ),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      'Create an account.',
+                                      style: GoogleFonts.montserrat(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w600,
+                                        color: const Color.fromARGB(
+                                          255,
+                                          73,
+                                          32,
+                                          255,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
