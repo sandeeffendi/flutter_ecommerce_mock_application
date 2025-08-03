d5f0011d5dadb8a57b9ddb2010b493fe4f418444
sandeeffendi <sande.effendi@gmail.com>
2025-08-03 18:36:06 +0700

    Completed: Splash Screen UI

--- lib/screens/mobile/splash_screen.dart
+++ lib/screens/mobile/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -6,7 +7,35 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/fresh_hub_logo.png'),
+            SizedBox(height: 10),
+            Container(
+              width: 200,
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              child: Center(
+                child: Text(
+                  'Fresh Hub',
+                  style: GoogleFonts.montserrat(
+                    fontSize: 25,
+                    color: Theme.of(context).colorScheme.surface,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
