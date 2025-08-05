863d038eab295ace3c32e3ddc86d0800a6eadc10
sandeeffendi <sande.effendi@gmail.com>
2025-08-05 13:36:45 +0700

    Add: Register Succes screen

---
+++ lib/screens/mobile/register_succes_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:my_submission_app/screens/mobile/login_page.dart';
+
+class RegisterSuccesScreen extends StatefulWidget {
+  const RegisterSuccesScreen({super.key});
+
+  @override
+  State<RegisterSuccesScreen> createState() => _RegisterSuccesState();
+}
+
+class _RegisterSuccesState extends State<RegisterSuccesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 300,
+                child: Lottie.asset(
+                  "assets/animation/success.json",
+                  repeat: false,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 35),
+                child: Column(
+                  children: [
+                    Text(
+                      'Account created!',
+                      style: Theme.of(context).textTheme.headlineLarge
+                          ?.copyWith(
+                            color: Theme.of(context).colorScheme.secondary,
+                          ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 10),
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(
+                            context,
+                          ).colorScheme.primaryContainer,
+                        ),
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MyLoginPage(),
+                            ),
+                          );
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              CupertinoIcons.back,
+                              color: Theme.of(context).colorScheme.surface,
+                              size: 25,
+                            ),
+                            Text(
+                              'Back to Login page',
+                              style: Theme.of(context).textTheme.labelLarge
+                                  ?.copyWith(
+                                    color: Theme.of(
+                                      context,
+                                    ).colorScheme.surface,
+                                  ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
