2ff71ee30b8bd5dd6e86b8d541c87e8d2c4d6acc
sandeeffendi <sande.effendi@gmail.com>
2025-08-07 21:32:33 +0700

    chore: minor changes

--- lib/screens/mobile/pages/basket_page.dart
+++ lib/screens/mobile/pages/basket_page.dart
@@ -4,6 +4,7 @@ class BasketPage extends StatelessWidget {
   const BasketPage({super.key});
 
   @override
+  //TODO: create fully functioning shopping cart
   Widget build(BuildContext context) {
     return Scaffold(body: Center(child: Text('Basket Page')));
   }
