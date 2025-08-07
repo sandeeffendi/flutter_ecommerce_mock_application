29cf269bfa4234bedece56e71d29b8e3122dd48c
sandeeffendi <sande.effendi@gmail.com>
2025-08-07 21:30:40 +0700

    fix: recommended feat

--- lib/screens/mobile/pages/shop_page.dart
+++ lib/screens/mobile/pages/shop_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:my_submission_app/components/my_category_list.dart';
 import 'package:my_submission_app/components/my_recommended_list.dart';
+import 'package:my_submission_app/model/background_color_list.dart';
 import 'package:my_submission_app/model/salad_item.dart';
 import 'package:my_submission_app/services/shared_prefs_services.dart';
 
@@ -12,7 +13,9 @@ class ShopPage extends StatefulWidget {
 }
 
 class _ShopPageState extends State<ShopPage> {
-  String _selectedValue = 'hottest';
+  // make random value for index
+  // final random = Random();
+  String _selectedValue = 'Hottest';
   String? username = '';
 
   Future<void> loadUsername() async {
@@ -31,130 +34,201 @@ class _ShopPageState extends State<ShopPage> {
 
   @override
   Widget build(BuildContext context) {
+    List<SaladItem> currentList;
+
+    if (_selectedValue == 'Hottest') {
+      currentList = saladItemHottestList;
+    } else if (_selectedValue == 'Popular') {
+      currentList = saladItemPopularList;
+    } else if (_selectedValue == 'New') {
+      currentList = saladItemNewList;
+    } else {
+      currentList = [];
+    }
+
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child:
-            // Shop Page Main Display
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Greeting Text
-                Padding(
-                  padding: EdgeInsetsGeometry.symmetric(horizontal: 10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Hello $username!',
-                        style: Theme.of(context).textTheme.headlineSmall,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8),
-                        child: Text(
-                          'What fruit\'s salad combo do you want today?',
-                          style: Theme.of(context).textTheme.labelLarge,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child:
+              // Shop Page Main Display
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Greeting Text
+                  Padding(
+                    padding: EdgeInsetsGeometry.symmetric(horizontal: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Hello $username!',
+                          style: Theme.of(context).textTheme.headlineSmall,
                         ),
-                      ),
-                    ],
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8),
+                          child: Text(
+                            'What fruit\'s salad combo do you want today?',
+                            style: Theme.of(context).textTheme.labelLarge,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
 
-                // Recommended Combo
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 30,
-                    horizontal: 10,
+                  // Recommended Combo
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 30,
+                      horizontal: 10,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Recommended Combo',
+                          style: Theme.of(context).textTheme.headlineSmall
+                              ?.copyWith(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 170,
+                          width: double.infinity,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              final SaladItem salad = saladItemList[index];
+                              return MyRecommendedList(
+                                name: salad.name,
+                                price: salad.price,
+                                imageAssets: salad.imageAssets,
+                              );
+                            },
+                            itemCount: saladItemList.length,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Recommended Combo',
-                        style: Theme.of(context).textTheme.headlineSmall
-                            ?.copyWith(fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: 170,
-                        width: double.infinity,
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemBuilder: (context, index) {
-                            final SaladItem salad = saladItemList[index];
-                            return MyRecommendedList(
-                              name: salad.name,
-                              price: salad.price,
-                              imageAssets: salad.imageAssets,
-                            );
+
+                  // Shop Item Category
+                  // TODO: Create Category NAV:  Hottest, Popular, and New Combo
+                  Center(
+                    child: Column(
+                      children: [
+                        SegmentedButton(
+                          emptySelectionAllowed: true,
+
+                          segments: [
+                            // Hottest Category
+                            ButtonSegment(
+                              value: 'Hottest',
+                              label: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(right: 3),
+                                      child: Icon(Icons.whatshot),
+                                    ),
+                                  ),
+                                  Text(
+                                    'Hottest',
+                                    style: Theme.of(
+                                      context,
+                                    ).textTheme.labelSmall,
+                                  ),
+                                ],
+                              ),
+                            ),
+
+                            // Popular Category
+                            ButtonSegment(
+                              value: 'Popular',
+                              label: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(right: 6),
+                                      child: Icon(Icons.trending_up),
+                                    ),
+                                  ),
+                                  Text(
+                                    'Popular',
+                                    style: Theme.of(
+                                      context,
+                                    ).textTheme.labelSmall,
+                                  ),
+                                ],
+                              ),
+                            ),
+
+                            // New Combo
+                            ButtonSegment(
+                              value: 'New',
+                              label: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: 3),
+                                    child: Icon(Icons.auto_awesome),
+                                  ),
+                                  Text(
+                                    'New',
+                                    style: Theme.of(
+                                      context,
+                                    ).textTheme.labelSmall,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                          selected: <String>{_selectedValue},
+                          onSelectionChanged: (Set<String> newSelection) {
+                            setState(() {
+                              _selectedValue = newSelection.first;
+                            });
                           },
-                          itemCount: saladItemList.length,
+                          showSelectedIcon: false,
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStateProperty.resolveWith((
+                              states,
+                            ) {
+                              if (states.contains(WidgetState.selected)) {
+                                return Theme.of(context).colorScheme.primary;
+                              }
+                              return Theme.of(context).colorScheme.tertiary;
+                            }),
+                          ),
                         ),
-                      ),
-                    ],
+
+                        //Category list
+                        SizedBox(
+                          height: 170,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              final ColorList color =
+                                  backgroundColorList[index %
+                                      backgroundColorList.length];
+                              final SaladItem item = currentList[index];
+                              return MyCategoryList(
+                                name: item.name,
+                                price: item.price,
+                                imageAssets: item.imageAssets,
+                                color: color.color,
+                              );
+                            },
+                            itemCount: currentList.length,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-
-                // Shop Item Category
-                // TODO: Create Category NAV:  Hottest, Popular, and New Combo
-                Center(
-                  child: SegmentedButton(
-                    segments: [
-                      // Hottest Category
-                      ButtonSegment(
-                        value: 'Hottest',
-                        label: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(right: 3),
-                              child: Icon(Icons.whatshot),
-                            ),
-                            Text('Hottest'),
-                          ],
-                        ),
-                      ),
-
-                      // Popular Category
-                      ButtonSegment(
-                        value: 'Popular',
-                        label: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(right: 3),
-                              child: Icon(Icons.trending_up),
-                            ),
-                            Text('Popular'),
-                          ],
-                        ),
-                      ),
-
-                      // New Combo
-                      ButtonSegment(
-                        value: 'New',
-                        label: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(right: 3),
-                              child: Icon(Icons.auto_awesome),
-                            ),
-                            Text('New'),
-                          ],
-                        ),
-                      ),
-                    ],
-                    selected: <String>{_selectedValue},
-                    onSelectionChanged: (Set<String> newSelection) {
-                      setState(() {
-                        _selectedValue = newSelection.first;
-                      });
-                    },
-                  ),
-                ),
-              ],
-            ),
+                ],
+              ),
+        ),
       ),
     );
   }
