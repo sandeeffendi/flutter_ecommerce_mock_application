f798236c9a4bdc5ba837375cc9a862208951bca8
sandeeffendi <sande.effendi@gmail.com>
2025-08-10 01:47:15 +0700

    feat: adding settings pages

---
+++ lib/screens/mobile/pages/setting_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:my_submission_app/theme/ThemeProvider.dart';
+import 'package:provider/provider.dart';
+
+class SettingPage extends StatelessWidget {
+  SettingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final themeProvider = Provider.of<Themeprovider>(context);
+
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 25),
+              child: Text(
+                'Settings',
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(25),
+              padding: EdgeInsets.symmetric(horizontal: 25),
+              width: double.infinity,
+              height: screenWidth > 600 ? 100 : 400,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Appearance',
+                        style: Theme.of(context).textTheme.labelLarge,
+                      ),
+                      Switch(
+                        value: themeProvider.isLightMode,
+                        onChanged: (value) {
+                          themeProvider.ToggleTheme();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
