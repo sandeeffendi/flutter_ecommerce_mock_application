83150dd3ba0f9e5303ed645d023f66791adbf301
sandeeffendi <sande.effendi@gmail.com>
2025-08-10 02:28:38 +0700

    chore: minor changes

--- lib/screens/mobile/pages/about_pages.dart
+++ lib/screens/mobile/pages/about_pages.dart
@@ -32,38 +32,40 @@ class AboutPages extends StatelessWidget {
             // Main Display
             Expanded(
               flex: 9,
-              child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(50),
-                    topRight: Radius.circular(50),
+              child: SingleChildScrollView(
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50),
+                    ),
+                    color: Theme.of(context).colorScheme.surface,
                   ),
-                  color: Theme.of(context).colorScheme.surface,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 50),
-                      child: Text(
-                        'About Fresh Hub',
-                        style: Theme.of(context).textTheme.headlineLarge,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 50),
+                        child: Text(
+                          'About Fresh Hub',
+                          style: Theme.of(context).textTheme.headlineLarge,
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        bottom: 120,
-                        left: 25,
-                        right: 25,
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          bottom: 120,
+                          left: 25,
+                          right: 25,
+                        ),
+                        child: Text(
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget sapien non nisi placerat laoreet. Integer nec odio et purus laoreet hendrerit. Suspendisse potenti. Curabitur ut dignissim purus. Nullam euismod, orci non convallis commodo, tortor orci faucibus purus, vel vehicula nibh odio at metus. Pellentesque vel dui sed ligula commodo sodales. Fusce in diam at justo egestas fringilla. Vestibulum ut hendrerit metus. Phasellus id velit sed odio faucibus eleifend. Etiam sollicitudin purus sed tincidunt dapibus. Vivamus sit amet orci id sapien pharetra fermentum. Ut id nisl sed purus interdum feugiat. Proin tincidunt sapien nec turpis luctus, eget facilisis ligula cursus. Sed vitae magna at libero pulvinar mattis. Curabitur vel felis non dolor hendrerit aliquet. Nunc et neque facilisis, ultricies nunc id, imperdiet erat. Praesent scelerisque purus vitae eros sollicitudin, sed convallis velit congue. Donec at erat ac velit blandit',
+                          style: Theme.of(context).textTheme.titleSmall,
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                      child: Text(
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget sapien non nisi placerat laoreet. Integer nec odio et purus laoreet hendrerit. Suspendisse potenti. Curabitur ut dignissim purus. Nullam euismod, orci non convallis commodo, tortor orci faucibus purus, vel vehicula nibh odio at metus. Pellentesque vel dui sed ligula commodo sodales. Fusce in diam at justo egestas fringilla. Vestibulum ut hendrerit metus. Phasellus id velit sed odio faucibus eleifend. Etiam sollicitudin purus sed tincidunt dapibus. Vivamus sit amet orci id sapien pharetra fermentum. Ut id nisl sed purus interdum feugiat. Proin tincidunt sapien nec turpis luctus, eget facilisis ligula cursus. Sed vitae magna at libero pulvinar mattis. Curabitur vel felis non dolor hendrerit aliquet. Nunc et neque facilisis, ultricies nunc id, imperdiet erat. Praesent scelerisque purus vitae eros sollicitudin, sed convallis velit congue. Donec at erat ac velit blandit',
-                        style: Theme.of(context).textTheme.titleSmall,
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
